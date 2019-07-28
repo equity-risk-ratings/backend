a0d86eb237322e95b0bba6d925d091d9ef799b23
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-28 11:12:40 +0100

    retrieve users endpoint

--- back-end/users/users-router.js
+++ back-end/users/users-router.js
@@ -2,3 +2,14 @@ const express = require('express');
 const Users = require('./users-model.js');
 const route = express.Router();
 
+route.get('/', async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the users',
+    });
+  }
+});
