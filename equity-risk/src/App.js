f17187cb246e54c4287ac0fc9db4fa2b159ce64c
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-29 18:13:13 +0100

    react boiler plate

---
+++ equity-risk/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Equity Risk Ratings</h2>
+    </div>
+  );
+}
+
+export default App;
