ddfcbbbfb0f2932c790197ce79cb51f6d6cbafc1
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-29 13:32:09 +0100

    refactor

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
 const usersRouter = require('./users/users-router.js');
 
 const server = express();
+
+server.use(helmet());
 server.use(express.json());
+server.use(cors());
+
+
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
