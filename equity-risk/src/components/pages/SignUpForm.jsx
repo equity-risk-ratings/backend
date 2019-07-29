9c96995159d7dc428dfbd9c8e4d4f58fa3736323
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-29 23:24:04 +0100

    SignUp Form

---
+++ equity-risk/src/components/pages/SignUpForm.jsx
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class SignUpForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      hasAgreed: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    let target = event.target;
+    let value = target.type === 'checkbox' ? target.checked : target.value;
+    let name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    console.log('The form was submitted with the following:');
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className='FormWrapper'>
+        <form
+          onSubmit={this.handleSubmit}
+          className='FormFields'>
+            <div className="FormField">
+              <label
+                className='FormField__Label'
+                htmlFor='firstName'>
+                  First Name
+              </label>
+              <input
+                type='text'
+                id='name'
+                className='FormField__Input'
+                placeholder='Enter your first name'
+                name='firstName'
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <div className="FormField">
+              <label
+                className='FormField__Label'
+                htmlFor='lastName'>
+                  Last Name
+              </label>
+              <input
+                type='text'
+                id='name'
+                className='FormField__Input'
+                placeholder='Enter your last name'
+                name='lastName'
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <div className="FormField">
+            <label
+                className='FormField__Label'
+                htmlFor='email'>
+                  E-mail Address
+            </label>
+            <input
+              type='email'
+              id='email'
+              className='FormField__Input'
+              placeholder='Enter your email'
+              name='email'
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+            </div>
+
+            <div className="FormField">
+              <label
+                className='FormField__Label'
+                htmlFor='password'
+              >
+                Password
+              </label>
+              <input
+                type='password'
+                id='password'
+                className='FormField__Input'
+                placeholder='Enter your password'
+                name='password'
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </div>
+        </form>
+      </div>
+    )
+  }
+}
