9f2883b6d5ab1bcdabc83f3a3322a554f25bf15b
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-29 13:59:17 +0100

    login endpoint

--- back-end/users/users-router.js
+++ back-end/users/users-router.js
@@ -32,6 +32,28 @@ router.post('/register', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  let { email, password } = req.body;
+
+  Users.findBy({ email })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.firstName}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
 function generateToken(user) {
   const payload = {
     sub: user.id,
