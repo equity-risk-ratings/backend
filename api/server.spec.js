39c0f26868cfc4dbf8ed8e2f0fcd0e10bffed0c8
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-31 12:46:53 +0100

    test for get users endpoint

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const server = require('./server.js');
+const db = require('../data/dbConfig.js');
 
 describe('server.js', () => {
   it('should set the test env', () => {
@@ -15,10 +16,27 @@ describe('server.js', () => {
   });
 
   describe('GET /api/user', () => {
-    it('should return users', async () => {
+    afterEach(async () => {
+      await db('users').truncate();
+    })
+
+    it('should hit endpoint', async () => {
       const res = await request(server).get('/api/users');
       expect(res.status).toBe(200);
       expect(res.body).toEqual([]);
+    });
+
+    it('should return all users in the db', async () => {
+      const users = [
+        { id: 1, firstName: 'Shola', lastName: 'Ayeni', email: 'ayenisholah@yahoo.com', password: 1234 },
+        { id: 2, firstName: 'Dare', lastName: 'Ayeni2', email: 'dareayeni@yahoo.com', password: 1234 }
+      ];
+
+      await db('users').insert(users);
+
+      const res = await request(server).get('/api/users');
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveLength(2);
     });
   });
   
