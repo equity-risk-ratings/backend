3f9dccd782f74c6be21fc897d27505019f008542
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-01 17:25:29 +0100

    migration to postgress

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,47 @@
+const env = require('dotenv');
+env.config();
+
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: 'postgresql',
     useNullAsDefault: true,
-    connection: {
-      filename: './data/database.db3',
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      },
+      min: 2,
+      max: 10
     },
     migrations: {
       directory: './data/migrations',
+      tableName: 'knex_migrations'
     },
     seeds: {
       directory: './data/seeds',
     },
   },
   testing: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/test.db3',
-    },
+    client: 'postgresql',
+    connection: process.env.TEST_DATABASE_URL,
     migrations: {
       directory: './data/migrations',
+      tableName: 'knex_migrations'
     },
     seeds: {
       directory: './data/seeds',
     },
+
+    production: {
+      client: 'postgrsql',
+      connection: process.env.DATABASE_URL,
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        directory: './data/migrations'
+      },
+      seeds: {
+        directory: './data/seeds'
+      }
+    }
   }
 };
