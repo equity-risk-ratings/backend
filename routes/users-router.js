0f83f18ec318b23fa9e3a0596b1a656aa802601b
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-01 09:09:12 +0100

    get individual user endpoint

--- routes/users-router.js
+++ routes/users-router.js
@@ -13,6 +13,23 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await Users.findById(req.params.id);
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Error retrieving '
+    });
+  }
+});
 
 
 module.exports = router;
