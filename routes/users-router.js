c3a0ec292927093ee806d8c540355ed23dfb29aa
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-01 01:04:16 +0100

    Users Router

---
+++ routes/users-router.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const Users = require('../models/users-model.js');
+
+
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the users',
+    });
+  }
+});
+
+
+
+module.exports = router;
