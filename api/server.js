b2f8729d556da2c64e0a0ab157b971d01b23d1c1
Shola Ayeni <ayenisholah@yahoo.com>
2019-08-02 08:31:16 +0100

    companies

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../routes/users-router.js');
+const companiesRouter = require('../routes/companies-router.js');
 
 const server = express();
 
@@ -13,6 +14,7 @@ server.use(cors());
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
+server.use('/api/companies', companiesRouter);
 
 
 server.get('/', (req, res) => {
