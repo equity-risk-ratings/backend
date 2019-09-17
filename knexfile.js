aa270781cdfbcabe051b569aa4ebd82e986e603b
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-17 16:58:59 +0100

    code refactor

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     useNullAsDefault: true,
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DB_CONNECTION_TEST,
     pool: {
       min: 2,
       max: 10
@@ -20,7 +20,7 @@ module.exports = {
   },
   testing: {
     client: 'postgresql',
-    connection: process.env.TEST_DATABASE_URL,
+    connection: process.env.DB_CONNECTION_TEST,
     migrations: {
       directory: './data/migrations',
       tableName: 'knex_migrations'
@@ -28,20 +28,19 @@ module.exports = {
     seeds: {
       directory: './data/seeds',
     },
-
-    production: {
-      client: 'postgrsql',
-      connection: process.env.DATABASE_URL,
-      pool: {
-        min: 2,
-        max: 10
-      },
-      migrations: {
-        directory: './data/migrations'
-      },
-      seeds: {
-        directory: './data/seeds'
-      }
+  },
+  production: {
+    client: 'postgrsql',
+    connection: process.env.DB_CONNECTION,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 };
