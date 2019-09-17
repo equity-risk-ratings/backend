03b34583a28a88b0208f1e506f29be266a2d42e9
Shola Ayeni <ayenisholah@yahoo.com>
2019-09-17 14:12:45 +0100

    refactor code base

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(logger('dev'));
+app.use(helmet());
+
+app.get('/', (req, res) => res.status(200).json({
+  success: true,
+  message: 'API is alive...'
+}));
+
+
 
 const server = require('./api/server.js');
 
